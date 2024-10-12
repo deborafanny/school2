405994a36d57c27d3edcce4f2dd551ae8318d81b
Fanny <fanigiya@gmail.com>
2024-10-12 18:38:39 +0900

    Update galeri.php

--- pages/galeri.php
+++ pages/galeri.php
@@ -9,18 +9,24 @@
     </div>
     <div class="row row-cols-1 row-cols-md-3 g-4">
       <div class="col">
-        <div class="card h-100">
-          <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Foto 1">
+        <div class="card h-100 text-center">
+          <img src="<?= base_url('assets/img/about/s-6.jpg'); ?>" class="card-img-top" alt="Foto 1">
+          <h5 class="card-title p-3">Rapat Komite</h5>
+          <p class="card-text"></p>
         </div>
       </div>
       <div class="col">
-        <div class="card h-100">
-          <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Foto 2">
+        <div class="card h-100 text-center">
+          <img src="<?= base_url('assets/img/about/s-7.jpg'); ?>" class="card-img-top" alt="Foto 2">
+          <h5 class="card-title p-3">Workshop</h5>
+          <p class="card-text"></p>
         </div>
       </div>
       <div class="col">
-        <div class="card h-100">
-          <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Foto 3">
+        <div class="card h-100 text-center">
+          <img src="<?= base_url('assets/img/about/s-8.jpg'); ?>" class="card-img-top" alt="Foto 3">
+          <h5 class="card-title p-3">Lomba Bersih & Hias Kelas</h5>
+          <p class="card-text"></p>
         </div>
       </div>
 
@@ -28,7 +34,7 @@
       <div class="col">
         <div class="card h-100">
           <div class="ratio ratio-16x9">
-            <iframe src="https://www.youtube.com/embed/VIDEO_ID" title="YouTube video" allowfullscreen></iframe>
+            <iframe src="https://www.youtube.com/embed/lt-IB88Al7Q" title="YouTube video" allowfullscreen></iframe>
           </div>
         </div>
       </div>
@@ -37,7 +43,7 @@
       <div class="col">
         <div class="card h-100">
           <div class="ratio ratio-16x9">
-            <iframe src="https://www.youtube.com/embed/VIDEO_ID" title="YouTube video" allowfullscreen></iframe>
+            <iframe src="https://www.youtube.com/embed/vBSshSvQt2Y" title="YouTube video" allowfullscreen></iframe>
           </div>
         </div>
       </div>
@@ -45,7 +51,7 @@
       <div class="col">
         <div class="card h-100">
           <div class="ratio ratio-16x9">
-            <iframe src="https://www.youtube.com/embed/VIDEO_ID" title="YouTube video" allowfullscreen></iframe>
+            <iframe src="https://www.youtube.com/embed/Fyv6xOvEB5k" title="YouTube video" allowfullscreen></iframe>
           </div>
         </div>
       </div>
